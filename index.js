4dc7001dcd00be34ac1264d6c0223c8385ce69bd
Lehotaiabel <lehotaiabel@elg-bp.edu.hu>
2022-11-12 10:38:18 +0100

    add files

---
+++ index.js
@@ -0,0 +1,12 @@
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
