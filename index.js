af0a75178e8f95c0c4c900d55e04975043498e9a
Lehotaiabel <lehotaiabel@elg-bp.edu.hu>
2022-11-18 01:34:03 +0100

    Update

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+$(function() {
+  $('#toogle-one').bootstrapToogle({
+      on: 'Enable',
+      off: 'Disable'
+  });
+})
+
+
+
+function myFunction() {
+  document.getElementById("demo").innerHTML =  "2023";
+}
+
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 if (toastTrigger) {
@@ -10,3 +23,12 @@ if (toastTrigger) {
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myDIV *").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
